b4de999e69bc4a352f069c0fadedbb19277aa9cc
박소은 <49473267+So-EunPark@users.noreply.github.com>
2019-12-18 22:30:10 +0900

    12/18

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20190909',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
 app = Flask(__name__)
 
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
